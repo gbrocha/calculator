6cd4011f069eca0ee406442afe6df58bf41b5f07
Gabriel Rocha <gbrrochagbr@gmail.com>
2021-06-19 20:09:15 -0300

    adding tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { FunctionComponent } from 'react'
+import './index.css'
 
 const App: FunctionComponent = () => {
-  return <h1>Hello</h1>
+  return <h1 className="text-purple-700">Hello</h1>
 }
 
 export default App
